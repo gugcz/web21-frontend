8f80d4512145e2314f995c956eb7a4fe679b11fd
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-04-16 16:20:18 +0200

    chore: add lint output to next dev log

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
   future: {
     webpack5: true,
+  },
+  webpack: (config) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new ESLintPlugin({ extensions: ['.js', '.jsx'] }));
+    // Important: return the modified config
+
+    return config;
   },
 };
