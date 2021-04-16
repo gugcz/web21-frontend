2dcc06b73a7d3a0f427a8733ec5bacd9c7584fa3
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-04-16 14:42:41 +0200

    refactor: make it more mobile first #3

--- components/layout/AppHeader/AppHeader.jsx
+++ components/layout/AppHeader/AppHeader.jsx
@@ -1,7 +1,8 @@
-import { AppBar, Box, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, Hidden, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 import Logo from '../../common/Logo/Logo';
 import { JoinUsHeaderButton } from '../JoinUsHeader/JoinUsHeaderButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(({ spacing }) => ({
   menu: {
@@ -12,6 +13,10 @@ const useStyles = makeStyles(({ spacing }) => ({
       padding: spacing(0, 1),
     },
   },
+  hamburgerMenu: {
+    marginRight: 0,
+    marginLeft: 'auto',
+  },
 }));
 
 export const AppHeader = () => {
@@ -21,13 +26,20 @@ export const AppHeader = () => {
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
         <Logo />
-        <Box m={1} className={classes.menu}>
-          <Typography>Menu 1</Typography>
-          <Typography>Menu 2</Typography>
-          <Typography>Menu 3</Typography>
-          <Typography>Menu 4</Typography>
-        </Box>
-        <JoinUsHeaderButton />
+        <Hidden smDown>
+          <Box m={1} className={classes.menu}>
+            <Typography>Menu 1</Typography>
+            <Typography>Menu 2</Typography>
+            <Typography>Menu 3</Typography>
+            <Typography>Menu 4</Typography>
+          </Box>
+          <JoinUsHeaderButton />
+        </Hidden>
+        <Hidden mdUp>
+          <IconButton edge="start" color="primary" aria-label="menu" className={classes.hamburgerMenu}>
+            <MenuIcon fontSize="large" />
+          </IconButton>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
