77e30223b25b5b49e9d910a104d32e0390ad2614
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-05-01 00:11:52 +0200

    feat(stats): extract to independent component #8

---
+++ components/landingPage/ScorecardsBox/ScorecardsBox.jsx
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import React from 'react';
+import PropTypes from 'prop-types';
+import ScoreCard from '../../common/ScoreCard/ScoreCard';
+import { HatIcon, KioskIcon, LocationIcon, PersonIcon } from '../../icons/icons';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+}));
+
+const ScorecardsBox = ({ stats }) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      {/* todo - mobile */}
+      <ScoreCard icon={HatIcon} label="Lidí jsme vzdělali" value={stats.visitors} iconSize={96} />
+      <ScoreCard icon={LocationIcon} label="Uspořádaných akcí" value={stats.events} iconSize={65} />
+      <ScoreCard icon={PersonIcon} label="Nových gugerů" value={stats.gugers} />
+      <ScoreCard icon={KioskIcon} label="Měst kde působíme" value={stats.cities} />
+    </Box>
+  );
+};
+
+ScorecardsBox.propTypes = {
+  stats: PropTypes.shape({
+    visitors: PropTypes.number,
+    events: PropTypes.number,
+    cities: PropTypes.number,
+    gugers: PropTypes.number,
+  }),
+};
+
+export default ScorecardsBox;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,20 @@
 import { Box, makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { PartnersSection } from '../components/sections/PartnersSection/PartnersSection';
-import ScoreCard from '../components/common/ScoreCard/ScoreCard';
-import { HatIcon, KioskIcon, LocationIcon, PersonIcon } from '../components/icons/icons';
 import homePageData from '../data/homePageData';
+import ScorecardsBox from '../components/landingPage/ScorecardsBox/ScorecardsBox';
 
 const useStyles = makeStyles(() => ({
   root: {},
-  scorecards: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
 }));
 
-const Home = ({ stats }) => {
+const Home = ({ data }) => {
   const classes = useStyles();
 
   return (
     <Box m={4} className={classes.root}>
       <Box>headlineIntro</Box>
-      <Box className={classes.scorecards}>
-        <ScoreCard icon={HatIcon} label="Lidí jsme vzdělali" value={stats.visitors} iconSize={96} />
-        <ScoreCard icon={LocationIcon} label="Uspořádaných akcí" value={stats.events} iconSize={65} />
-        <ScoreCard icon={PersonIcon} label="Nových gugerů" value={stats.gugers} />
-        <ScoreCard icon={KioskIcon} label="Měst kde působíme" value={stats.cities} />
-      </Box>
+      <ScorecardsBox stats={data.stats} />
       <Box>events</Box>
       <Box>connection</Box>
       <Box>písmenka</Box>
@@ -38,17 +27,14 @@ const Home = ({ stats }) => {
 };
 
 Home.propTypes = {
-  stats: PropTypes.shape({
-    visitors: PropTypes.number,
-    events: PropTypes.number,
-    cities: PropTypes.number,
-    gugers: PropTypes.number,
+  data: PropTypes.shape({
+    stats: PropTypes.object,
   }),
 };
 
 export async function getStaticProps() {
   return {
-    props: { stats: homePageData.stats },
+    props: { data: homePageData },
   };
 }
 
