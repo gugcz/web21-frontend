4bc79e8afa7fee00903a9445944b1f57d9344fd8
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-07-06 13:01:39 +0200

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   webpack: (config) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
