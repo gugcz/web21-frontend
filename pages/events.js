e75379657f92877d6a4cde0667c3a34e954d4503
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-08-09 17:58:17 +0200

    feat: prepare data #28

--- pages/events.js
+++ pages/events.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
 import EventBox from '../components/events/EventBox/EventBox';
 import EventsHeadline from '../components/events/EventsHeadline/EventsHeadline';
+import events from '../data/eventsData';
 
-export default function Events() {
+const Events = ({ events }) => {
   return (
     <Box m={4}>
       <EventsHeadline />
       <Box>filters</Box>
-      <Box>
-        events
-        <EventBox slug="prvni-event" />
-        <EventBox slug="druhy-event" />
-        <EventBox slug="treti-event" />
-        <EventBox slug="ctvrty-event" />
-      </Box>
+      <Box>{events && events.map((event) => <EventBox {...event} key={event.slug} />)}</Box>
     </Box>
   );
+};
+
+Events.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      stats: PropTypes.object,
+      chapterId: PropTypes.any,
+      chapterName: PropTypes.any,
+      eventName: PropTypes.any,
+      date: PropTypes.any,
+      address: PropTypes.any,
+      slug: PropTypes.any,
+      intro: PropTypes.any,
+      location: PropTypes.any,
+      image: PropTypes.any,
+      regLink: PropTypes.any,
+      mdText: PropTypes.any,
+      organizers: PropTypes.any,
+      eventId: PropTypes.any,
+      chapterCode: PropTypes.any,
+    })
+  ),
+};
+
+export async function getStaticProps() {
+  return {
+    props: { events: events },
+  };
 }
+
+export default Events;
