69ba3e6b7e2e203763da4456826c18f4493cc71f
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-08-10 08:38:27 +0200

    feat: style box - wip #28

--- components/events/EventBox/EventBox.jsx
+++ components/events/EventBox/EventBox.jsx
@@ -1,11 +1,21 @@
-import { makeStyles, Typography } from '@material-ui/core';
+import { Button, makeStyles, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import React from 'react';
 import PropTypes from 'prop-types';
 import NextLink from '../../common/NextLink/NextLink';
 
+export const EventBoxPropTypes = PropTypes.shape({
+  slug: PropTypes.string,
+});
+
 const useStyles = makeStyles(() => ({
-  root: {},
+  root: {
+    border: '1.5px solid #00AF41',
+    borderRadius: 6,
+  },
+  rvsp: {
+    width: '80%',
+  },
 }));
 
 const EventBox = ({ slug }) => {
@@ -18,13 +28,17 @@ const EventBox = ({ slug }) => {
   return (
     <Box className={classes.root}>
       <Typography>Event info with link</Typography>
-      <NextLink href={`/event/${encodeURIComponent(slug)}`}>S odkazem do události {slug}</NextLink>
+      <Button
+        className={classes.rvsp}
+        variant="contained"
+        component={NextLink}
+        href={`/event/${encodeURIComponent(slug)}`}>
+        Chytit Covid na eventu
+      </Button>
     </Box>
   );
 };
 
-EventBox.propTypes = {
-  slug: PropTypes.string,
-};
+EventBox.propTypes = EventBoxPropTypes;
 
 export default EventBox;
