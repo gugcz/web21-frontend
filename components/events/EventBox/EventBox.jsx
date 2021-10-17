5f253a4ba00c1f8a6be4fc269d8165713484887b
Lukáš Przezek <lukas.przeczek@commity.cz>
2021-10-17 18:36:37 +0200

    finish event box

--- components/events/EventBox/EventBox.jsx
+++ components/events/EventBox/EventBox.jsx
@@ -1,40 +1,116 @@
-import { Button, makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import React from 'react';
 import PropTypes from 'prop-types';
-import NextLink from '../../common/NextLink/NextLink';
+import { CalendarToday as CalendarIcon, Place as PlaceIcon, Schedule as TimeIcon } from '@material-ui/icons';
+import dayjs from 'dayjs';
+import classNames from 'classnames/bind';
+import ChapterLogo from '../../common/ChapterLogo/ChapterLogo';
+import DetailButton from '../../common/DetailButton/DetailButton';
 
-export const EventBoxPropTypes = PropTypes.shape({
-  slug: PropTypes.string,
-});
+export const EventBoxPropTypes = {
+  event: PropTypes.shape({
+    slug: PropTypes.string,
+    chapterId: PropTypes.string,
+    chapterName: PropTypes.string,
+    chapterCode: PropTypes.string,
+    eventName: PropTypes.string,
+    date: PropTypes.string,
+    address: PropTypes.string,
+    intro: PropTypes.string,
+    location: PropTypes.string,
+    image: PropTypes.string,
+    regLink: PropTypes.string,
+    mdText: PropTypes.string,
+    eventId: PropTypes.number,
+    timeStart: PropTypes.string,
+    timeEnd: PropTypes.string,
+  }),
+};
 
-const useStyles = makeStyles(() => ({
-  root: {
-    border: '1.5px solid #00AF41',
-    borderRadius: 6,
-  },
-  rvsp: {
-    width: '80%',
-  },
-}));
+const useStyles = (chapterCode) =>
+  makeStyles(({ spacing, palette }) => ({
+    root: {
+      border: '1.5px solid',
+      borderColor: palette.chapters[chapterCode],
+      borderRadius: 6,
+      padding: spacing(2),
+    },
+    image: {
+      width: '100%',
+    },
+    eventName: {
+      fontWeight: 'bold',
+      fontSize: 21,
+      margin: spacing(2, 0),
+    },
+    intro: {
+      margin: spacing(3, 0),
+      color: '#9A9A9A',
+    },
+    timeAndDateWrapper: {
+      margin: spacing(2, 0),
+      display: 'flex',
+      flexDirection: 'row',
+      color: '#6A6A6A',
+    },
+    iconWithText: {
+      display: 'flex',
+      flexDirection: 'row',
+      marginRight: spacing(3),
+      '&>p': {
+        fontWeight: 500,
+      },
+    },
+    locationWrapper: {
+      margin: spacing(2, 0),
+    },
+    redPlace: {
+      color: 'red',
+    },
+    rvsp: {
+      // todo - align button with end of box in grid
+    },
+  }));
 
-const EventBox = ({ slug }) => {
-  const classes = useStyles();
+const EventBox = ({ event }) => {
+  const classes = useStyles(event.chapterCode.toLowerCase())();
 
-  if (!slug) {
+  if (!event) {
     return null;
   }
 
   return (
-    <Box className={classes.root}>
-      <Typography>Event info with link</Typography>
-      <Button
+    <Box className={classes.root} boxShadow={2}>
+      <ChapterLogo variant={event.chapterCode} />
+      <img src={event.image} className={classes.image} />
+      <Typography variant="h4" className={classes.eventName}>
+        {event.eventName}
+      </Typography>
+      <Typography variant="body1" className={classes.intro}>
+        {event.intro}
+      </Typography>
+      <box className={classes.timeAndDateWrapper}>
+        <box className={classes.iconWithText}>
+          <CalendarIcon />
+          <Typography>{dayjs(event.date).format('DD.MM.YY')}</Typography>
+        </box>
+        <box className={classes.iconWithText}>
+          <TimeIcon />
+          <Typography>
+            {event.timeStart} - {event.timeEnd}
+          </Typography>
+        </box>
+      </box>
+      <box className={classNames(classes.locationWrapper, classes.iconWithText)}>
+        <PlaceIcon className={classes.redPlace} />
+        <Typography>{event.address}</Typography>
+      </box>
+      <DetailButton
         className={classes.rvsp}
-        variant="contained"
-        component={NextLink}
-        href={`/event/${encodeURIComponent(slug)}`}>
-        Chytit Covid na eventu
-      </Button>
+        href={`/event/${encodeURIComponent(event.slug)}`}
+        text="Chytit místo na akci"
+      />
     </Box>
   );
 };
--- pages/events.js
+++ pages/events.js
@@ -1,40 +1,64 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
 import EventBox from '../components/events/EventBox/EventBox';
 import EventsHeadline from '../components/events/EventsHeadline/EventsHeadline';
 import events from '../data/eventsData';
 
+const useStyles = makeStyles(({ spacing, breakpoints }) => ({
+  eventWrapper: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, minmax(377px, 1fr))',
+    gridTemplateRows: 'auto',
+    '&>*': {
+      margin: spacing(2),
+    },
+    [breakpoints.down('sm')]: {
+      gridTemplateColumns: '1fr',
+      '&>*': {
+        margin: spacing(2, 0),
+      },
+    },
+  },
+}));
+
 const Events = ({ events }) => {
+  const classes = useStyles();
+
   return (
     <Box m={4}>
       <EventsHeadline />
       <Box>filters</Box>
-      <Box>{events && events.map((event) => <EventBox {...event} key={event.slug} />)}</Box>
+      <Box className={classes.eventWrapper}>
+        {events && events.map((event) => <EventBox event={event} key={event.slug} />)}
+      </Box>
     </Box>
   );
 };
 
+export const singleEventPropType = {
+  stats: PropTypes.object,
+  chapterId: PropTypes.any,
+  chapterName: PropTypes.any,
+  eventName: PropTypes.any,
+  date: PropTypes.any,
+  address: PropTypes.any,
+  slug: PropTypes.any,
+  intro: PropTypes.any,
+  location: PropTypes.any,
+  image: PropTypes.any,
+  regLink: PropTypes.any,
+  mdText: PropTypes.any,
+  organizers: PropTypes.any,
+  eventId: PropTypes.any,
+  chapterCode: PropTypes.any,
+  timeStart: PropTypes.any,
+  timeEnd: PropTypes.any,
+};
+
 Events.propTypes = {
-  events: PropTypes.arrayOf(
-    PropTypes.shape({
-      stats: PropTypes.object,
-      chapterId: PropTypes.any,
-      chapterName: PropTypes.any,
-      eventName: PropTypes.any,
-      date: PropTypes.any,
-      address: PropTypes.any,
-      slug: PropTypes.any,
-      intro: PropTypes.any,
-      location: PropTypes.any,
-      image: PropTypes.any,
-      regLink: PropTypes.any,
-      mdText: PropTypes.any,
-      organizers: PropTypes.any,
-      eventId: PropTypes.any,
-      chapterCode: PropTypes.any,
-    })
-  ),
+  events: PropTypes.arrayOf(PropTypes.shape(singleEventPropType)),
 };
 
 export async function getStaticProps() {
