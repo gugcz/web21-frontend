47dd8108656e144ff5abde99e89b8521bebb9fde
Lukáš Przezek <lukas.przeczek@commity.cz>
2022-03-15 18:05:22 +0100

    feat(newsletter): Hay! the leads are now online!

--- components/NesletterForm/NesletterForm.jsx
+++ components/NesletterForm/NesletterForm.jsx
@@ -1,13 +1,14 @@
 import { Button, makeStyles, TextField, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import classNames from 'classnames/bind';
 import EmailIcon from '../icons/EmailIcon';
 import FacebookIcon from '../icons/FacebookIcon';
 import InstagramIcon from '../icons/InstagramIcon';
 import InfoText from '../common/InfoText/InfoText';
 import NextLink from '../common/NextLink/NextLink';
 
-const useStyles = makeStyles(({ spacing }) => ({
+const useStyles = makeStyles(({ spacing, palette }) => ({
   root: {
     width: '100%',
     display: 'flex',
@@ -55,11 +56,56 @@ const useStyles = makeStyles(({ spacing }) => ({
     borderRadius: '0 50px 50px 0',
     paddingRight: 24,
     boxShadow: 'none',
+    maxHeight: 48,
+  },
+  statusText: {
+    margin: spacing(2),
+    color: palette.primary.main,
+  },
+  error: {
+    color: palette.error.main,
   },
 }));
 
+const validate = (email) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
 export const NewsletterForm = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+  const [statusText, setStatusText] = useState();
+
+  const handleEmailValueChange = useCallback(
+    (event) => {
+      const value = event.target.value;
+      setEmail(value);
+    },
+    [setEmail]
+  );
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      if (validate(email)) {
+        setError(false);
+        fetch(`https://hook.eu1.make.com/dam53dm2r21iezb9hh7gcl4iotrkmjgy?email=${email}`, {
+          method: 'GET',
+        })
+          .then(() => setStatusText('Váš email byl úspěšně přidán do databáze. Díky!'))
+          .catch(() => {
+            setError(true);
+            setStatusText('Na internetu se něco nepovedlo. Zkuste to znovu a když se to nezlepší napište nám!');
+          });
+        //send request!
+      } else {
+        setError('Zadejte email. Díky!');
+      }
+    },
+    [email, setError, setStatusText]
+  );
 
   return (
     <Box className={classes.root}>
@@ -69,19 +115,26 @@ export const NewsletterForm = () => {
         <Typography variant={'body2'}>Neunikne ti žádný event</Typography>
         <Typography variant={'body2'}>Max 2 maily měsíčně, nic na sílu</Typography>
       </div>
-      <form className={classes.form} noValidate autoComplete="off">
+      <form className={classes.form} noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
+          error={error && !statusText}
+          helperText={error}
           className={classes.field}
           InputProps={{ classes: { root: classes.inputRoot, input: classes.inputInput } }}
           InputLabelProps={{ classes: { root: classes.inputInput } }}
+          onChange={handleEmailValueChange}
+          value={email}
           variant={'filled'}
           label={'Zadejte email'}
           margin="dense"
         />
-        <Button className={classes.button} variant={'contained'} color={'primary'}>
+        <Button className={classes.button} variant={'contained'} color={'primary'} type="submit">
           Přihlásit se k odběru
         </Button>
       </form>
+      {statusText && (
+        <Typography className={classNames(classes.statusText, { [classes.error]: error })}>{statusText}</Typography>
+      )}
       <div className={classes.socky}>
         <Typography variant={'body2'}>A taky socky</Typography>
         <NextLink href="https://www.facebook.com/GUG.cz">
