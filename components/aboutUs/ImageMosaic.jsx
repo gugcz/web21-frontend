7501aae5d35a71289399bdfcb60a5fc6d18253c8
Lukáš Przezek <lukas.przeczek@commity.cz>
2022-05-01 11:43:03 +0200

    WIP
    next steps:
    - blue and red shape
    - positions

--- components/aboutUs/ImageMosaic.jsx
+++ components/aboutUs/ImageMosaic.jsx
@@ -6,15 +6,45 @@ import { logoVariant } from '../common/ChapterLogo/ChapterLogo';
 
 const useStyles = makeStyles(({ spacing, breakpoints }) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    [breakpoints.down('sm')]: {
-      margin: spacing(0, 0, 2),
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplateRows: 'repeat(6, 1fr)',
+    gridGap: 32,
+    aspectRatio: 1,
+    maxWidth: '80%',
+    margin: '0 auto',
+    '& > *:nth-child(1)': {
+      gridColumnStart: 1,
+      gridColumnEnd: 3,
+      gridRowStart: 1,
+      gridRowEnd: 4,
     },
+    '& > *:nth-child(2)': {
+      gridColumnStart: 3,
+      gridColumnEnd: 5,
+      gridRowStart: 2,
+      gridRowEnd: 4,
+    },
+    '& > *:nth-child(3)': {},
+    '& > *:nth-child(4)': {},
   },
   img: {
     maxWidth: '100%',
+  },
+  bubbleShape: {
+    borderRadius: '50%',
+    borderTopRightRadius: 0,
+    width: '100%',
+    objectFit: 'cover',
+    aspectRatio: 1,
+  },
+  penguinShape: {
+    borderRadius: '30%',
+    borderTopLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    width: '100%',
+    objectFit: 'cover',
+    aspectRatio: 4 / 5,
   },
 }));
 
@@ -26,10 +56,17 @@ const ImageMosaic = () => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
-      <Circle size={'S'} variant={logoVariant.GXG} />
+    <>
+      <Box className={classes.root}>
+        <img src="/images/dev/eventBox.png" alt="random alt text" className={classes.bubbleShape} />
+        <img src="/images/dev/eventBox.png" alt="random alt text" className={classes.penguinShape} />
+        <Circle size={'M'} variant={logoVariant.GDG} />
+        <Circle size={'S'} variant={logoVariant.GBG} />
+        <Circle size={'S'} variant={logoVariant.GEG} />
+        <Circle size={'M'} variant={logoVariant.GXG} />
+      </Box>
       <img src="/images/hp/mosaic.png" alt="Photo mosaic" className={classes.img} />
-    </Box>
+    </>
   );
 };
 
