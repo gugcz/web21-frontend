7964c7288c0073fb457cbce5794a2d16c00ad3d4
Dominik Šimoník <dominik.simonik@gmail.com>
2023-12-18 21:07:51 +0100

    fix: naming

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Gug.cz - Vzděláváme česko v technologiích</title>
+        <title>GUG.cz - Vzděláváme Česko v technologiích</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
